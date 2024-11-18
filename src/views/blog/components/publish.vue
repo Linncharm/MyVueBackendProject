56365dbc0e7cae21bd06a9efc21162a364dcfb78
Linncharm <linncharm9@gmail.com>
2024-11-18 12:38:27 +0800

    Complete the markdown editor block

--- src/views/blog/components/publish.vue
+++ src/views/blog/components/publish.vue
@@ -1,13 +1,16 @@
 <template>
   <div class="vditor-container">
-    <div id="vditor" style="width: 100% ; height: 80%"></div>
+    <div id="vditor"></div>
   </div>
 </template>
 
 <script setup>
-import { onMounted } from "vue";
+import { onMounted , ref } from "vue";
 import Vditor from "vditor";
 import "vditor/dist/index.css";
+import { Setting } from "@element-plus/icons-vue";
+
+const delay = ref(100);
 
 defineProps({
   content: {
@@ -20,11 +23,92 @@ defineEmits(["update:content"]);
 
 onMounted(() => {
   console.log("Vditor mounted");
-  new Vditor("vditor", {
-    height: 300,
+  const vditor = new Vditor("vditor", {
+    height: "90%",
+    width: "100%",
     mode: "sv",
+    toolbar: [
+      "emoji",
+      "headings",
+      "bold",
+      "italic",
+      "strike",
+      "link",
+      "|",
+      "list",
+      "ordered-list",
+      "check",
+      "outdent",
+      "indent",
+      "|",
+      "quote",
+      "line",
+      "code",
+      "inline-code",
+      "insert-before",
+      "insert-after",
+      "|",
+      "upload",
+      "table",
+      "|",
+      "undo",
+      "redo",
+      "|",
+      "fullscreen",
+      "edit-mode",
+      {
+        name: "more",
+        toolbar: [
+          "both",
+          "code-theme",
+          "content-theme",
+          "export",
+          "outline",
+          "preview",
+        ],
+      },
+      /*{
+        name: "customize",
+        tip: "自定义工具栏",
+        className: "right",
+        icon: Setting,
+        click:()=>{
+          console.log("customize");
+          //alert("输入自定义的debounce时间，单位毫秒");
+          const input = prompt("输入自定义的debounce时间，单位毫秒", delay.value);
+
+          const newDelay = parseInt(input, 10);
+          if (!isNaN(newDelay) && newDelay > 0) {
+            delay.value = newDelay;
+            alert(`新的 debounce 时间已设置为：${newDelay} 毫秒`);
+          } else {
+            alert("请输入有效的正整数！");
+          }
+          console.log("delay", delay.value);
+        },
+      }*/
+    ],
+    preview:{
+      //预览 debounce 毫秒间隔
+      delay: delay.value,
+      mode: "both",
+      //暂时不知道有什么用
+      actions: ["desktop"],
+      theme: {}
+    },
+    counter:{
+      enable: true,
+    },
+    resize: {
+      enable: true,
+      position: "top",
+    },
+    theme: "classic",
   });
+
+  //vditor.setTheme("dark");
 });
+//vditor.setTheme("dark");
 </script>
 
 <style>
