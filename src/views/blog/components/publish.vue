8cd2ffba921b47f0b5f5a08ed4b0da36fc74bf12
Linncharm <linncharm9@gmail.com>
2024-11-19 11:59:18 +0800

    patch

--- src/views/blog/components/publish.vue
+++ src/views/blog/components/publish.vue
@@ -2,11 +2,29 @@
   <div class="vditor-container">
     <div id="vditor"></div>
     <el-dialog
-        title="发布文章"
-        :visible="blogDialogVisible"
+        title="保存文章"
+        v-model="blogDialogVisible"
         width="80%"
+        height="90%"
     >
+      <span>This is a message</span>
+        <el-form :model="blogPublishForm" style="margin-top: 10px">
+          <el-form-item>
+            <div style="display: flex;flex-direction: row;justify-content: space-around;flex-grow: 1;align-items: center">
+              <el-input v-model="blogPublishForm.title" placeholder="请输入文章标题" style="width: 45%;"></el-input>
+              <el-input v-model="blogPublishForm.author" placeholder="请输入文章作者" style="width: 45%;"></el-input>
+            </div>
+          </el-form-item>
+        </el-form>
 
+      <template #footer>
+        <div class="dialog-footer">
+          <el-button @click="blogDialogVisible = false">Cancel</el-button>
+          <el-button type="primary" @click="blogDialogVisible = false">
+            Confirm
+          </el-button>
+        </div>
+      </template>
     </el-dialog>
   </div>
 
@@ -14,10 +32,19 @@
 </template>
 
 <script setup>
-import { onMounted , ref } from "vue";
+import {onMounted, reactive, ref} from "vue";
 import Vditor from "vditor";
 import "vditor/dist/index.css";
 import { Setting } from "@element-plus/icons-vue";
+
+const blogPublishForm = reactive({
+  title: "",
+  author: "",
+  description: "",
+  tags: [],
+  category: "",
+  content: "",
+})
 
 const delay = ref(100);
 
@@ -38,15 +65,16 @@ onMounted(() => {
     mode: "sv",
     toolbar: [
       {
-        name: "customize",
-        tip: "发布文章",
-
+        name: "save",
+        tip: "保存文章",
+        tipPosition: "s",
         className: "right",
         //icon的svg设置，直接从svg文件里面复制即可
         icon: '<?xml version="1.0" ?><svg height="32" viewBox="-3 3 40 40" width="32" xmlns="http://www.w3.org/2000/svg"><path d="M0 0h48v48h-48z" fill="none"/><path d="M10 8v4h28v-4h-28zm0 20h8v12h12v-12h8l-14-14-14 14z"/></svg>',
         click: () => {
           //需要用div包裹el-dialog，否则会报错，因为组件根节点为非dom节点
           blogDialogVisible.value = true;
+          console.log("blogDialogVisible", blogDialogVisible.value);
         },
       },
       "|",
@@ -129,10 +157,24 @@ onMounted(() => {
   });
 
   //vditor.setTheme("dark");
+  //setTipPositionForAllTools(toolbar, 's');
 });
 //vditor.setTheme("dark");
 
 const blogDialogVisible = ref(false);
+
+/*
+
+const setTipPositionForAllTools = (tools, position) => {
+  tools.forEach((tool, index) => {
+    if (typeof tool === "object" && tool !== null) {
+      // 设置 tipPosition
+      tool.tipPosition = position;
+    }
+  });
+  console.log("setTipPositionForAllTools", tools);
+};
+*/
 
 </script>
 
